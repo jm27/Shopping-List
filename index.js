060ca5634993a06e25286b23f1fbbf28e4564982
Jesus Esquer <jme2791@gmail.com>
2020-07-10 21:02:56 -0700

    created initial files installed parcel-bundler

---
+++ index.js
@@ -0,0 +1,11 @@
+const shoppingForm = document.querySelector(".shopping");
+const list = document.querySelector(".list");
+
+// We will store our state in ouur array
+const items = [];
+
+function handleSubmit() {
+  e.preventDefault();
+}
+
+shoppingForm.addEventListener("submit", handleSubmit);
